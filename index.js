2308a1cbc284562b8d36db54af8896a63c75742e
Raphael Bezerra <raphabzerra15@gmail.com>
2026-09-16 21:36:53 -0300

    Corrige inport e padroniza nome dos módulos

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import verificarListaVazia from "./scripts/verificarListaVazia.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
-
 botaoAdicionar.addEventListener("click", (evento) => {
-    evento.preventDefault();
-    const itemDaLista = criarItemDaLista();
-    listaDeCompras.appendChild(itemDaLista)
-    verificarListaVazia(listaDeCompras);
-})
-
+  evento.preventDefault();
+  const itemDaLista = criarItemDaLista();
+  listaDeCompras.appendChild(itemDaLista);
+  verificarListaVazia(listaDeCompras);
+});
 
 verificarListaVazia(listaDeCompras);
